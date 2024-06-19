d7836f30633918d53fc58454eb335553795baa13
sai Varma <saivarma2710@gmail.com>
2024-06-19 20:46:00 +0530

    structured format

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_package
+setup(
+    name='mcq_generator',
+    author='Krishna Varma',
+    author_email='saivarma2710@gmail.com',
+    version='0.0.1',
+    packages=find_package()
+)
