4aabe2495297395d143460e1d54e970a781e7a1a
sai Varma <saivarma2710@gmail.com>
2024-06-19 20:52:16 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup,find_package
+from setuptools import setup,find_packages
 setup(
     name='mcq_generator',
     author='Krishna Varma',
     author_email='saivarma2710@gmail.com',
     version='0.0.1',
-    packages=find_package()
+    packages=find_packages()
 )
